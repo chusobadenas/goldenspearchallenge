7d2e0226bc7e39180bb481f44cf052685bc808c5
Jesús Badenas <chusobadenas@gmail.com>
2020-10-10 17:43:38 +0200

    Define Splash

---
+++ app/src/main/java/com/jesusbadenas/goldenspearchallenge/App.kt
@@ -0,0 +1,5 @@
+package com.jesusbadenas.goldenspearchallenge
+
+import android.app.Application
+
+class App: Application()
---
+++ app/src/main/java/com/jesusbadenas/goldenspearchallenge/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.jesusbadenas.goldenspearchallenge.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jesusbadenas.goldenspearchallenge.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+    }
+}
